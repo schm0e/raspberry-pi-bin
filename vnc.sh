e8e79d33a17aa00ae04a508158d2abca7454b34d
Dave Turner <dave@flyingmonkeysoftware.com>
2017-02-20 13:01:29 -0600

    Create vnc.sh
    initial commit

---
+++ vnc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#vncserver :1 -geometry 1920x1080 -depth 24 -dpi 96
+#vncserver :1 -geometry 1024x768 -depth 24 -dpi 96
+vncserver :1 -geometry 800x600 -depth 24 -dpi 96
+
+#vncserver -kill :1
